a61f5f774ef79516d6e034179c032889ba241fd2
s53034 <farojanu0911@gmail.com>
2023-03-02 11:59:29 +0100

    my dinnerparty

--- src/Party/DinnerParty.java
+++ src/Party/DinnerParty.java
@@ -1,7 +1,44 @@
 package Party;
 
+import java.util.Scanner;
+
 public class DinnerParty {
     public static void main(String[] args) {
+        // Here I am going to invite my friends to my Dinner Party which is going to be funny and amazing.
 
+        System.out.println("check first if you are allowed to join my dinner party");
+
+//
+        System.out.println("Please enter your age");
+        int age = 12;
+
+
+        if (age >= 18) {
+            System.out.println("Welcome on Board");
+        } else {
+            System.out.println("Better Luck Next Time");
+
+//        }
+//
+//        System.out.println( "Please write your name");
+//        Scanner sc = new Scanner (System.in);
+//        String name = sc.next();
+//        System.out.println("Hello " + name + " Welcome on Board!");
+
+
+// initializing my variables
+
+//        System.out.println("average age of the guests at my party");
+//        int guest1 = 18;
+//        int guest2 = 19;
+//        int guest3 = 22;
+//        int guest4 = 24;
+//        int guest5 = 23;
+//        int guest6 = 25;
+//        int average = (guest1 + guest2 + guest3 + guest5 + guest6) / 6 ;
+//        System.out.println(average);
+
+
+        }
     }
 }
